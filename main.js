42453852c8fa3db9cd0a3ccb5876fa58835f6678
zacky420 <albertoserrano7@hotmail.com>
2024-03-26 12:51:32 +0100

    avances importantes en la API y en el diseño

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-pokenombre = document.getElementsByClassName("PokeName")
+pokenombre = document.getElementsByClassName("PokeName");
+poketype1= document.getElementsByClassName("poketype1");
+poketype2= document.getElementsByClassName("poketype2");
+Pokecode = document.getElementsByClassName("PokeCode")
+pokeimg= document.getElementsByClassName("PokImg")
+
+
+
 
 let API_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 fetch(API_URL)
-  .then(res => res.json())
-  .then(data => { console.log(data);
-  
-  let pokename = data.results[0]
-  console.log(pokename)
-  
-})
-    
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
 
+    let pokearray = data.results;
+    console.log(pokearray);
+    
+    pokearray.forEach(pokemon => {
+      PokeUrl=pokemon.url;
+      PokName=pokemon.name
+      
+     
+
+
+
+
+
+      fetch(PokeUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+
+        
+
+        
+
+
+
+
+
+
+
+      })
+    });;
+
+  });
