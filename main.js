5da4709c47505ebf57e5c3250a195c7811153e86
zacky420 <albertoserrano7@hotmail.com>
2024-03-28 11:56:46 +0100

    finalizamos PokeApi funcional, Arreglando los colores de los botones, El JS para clasificar y quitando el HTML de las cards(ya que no es necesario

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
-
-let API_URL = "https://pokeapi.co/api/v2/pokemon/";
+let API_URL = "https://pokeapi.co/api/v2/pokemon/?offset=200&limit=200";
+let container = document.querySelector(".AllCards");
 
 fetch(API_URL)
   .then((res) => res.json())
@@ -8,53 +7,98 @@ fetch(API_URL)
     let pokearray = data.results;
 
     pokearray.forEach((pokemon) => {
-      PokeUrl = pokemon.url;
+      let PokeUrl = pokemon.url;
 
       fetch(PokeUrl)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           let Pokname = data.name;
           let PokId = data.id;
           let PokImg = data.sprites.front_default;
           let Poktype1 = data.types[0].type.name;
-          let Poktype2 = data.types[1] ? data.types[1].type.name : null
-          
-          let card = document.createElement('div');
-          card.classList.add("card","Pokecard");
-          card.style.width= "20rem";
-          
-          let img = document.createElement("img")
-          card.classList.add("card-img-top","PokImg");
+          let Poktype2 = data.types[1] ? data.types[1].type.name : null;
+
+          let card = document.createElement("div");
+          card.classList.add("card", "Pokecard");
+          card.style.width = "15rem";
+
+          let img = document.createElement("img");
+          img.classList.add("card-img-top", "PokImg");
           img.src = PokImg;
           img.alt = "Pokemon Image";
 
           let cardBody = document.createElement("div");
-          card.classList.add("card-body");
+          cardBody.classList.add("card-body");
 
           let title = document.createElement("h5");
           title.classList.add("PokeName");
           title.textContent = Pokname;
 
           let Pokecode = document.createElement("h6");
-          Pokecode.classList.add("card-title","PokeCode");
+          Pokecode.classList.add("card-title", "PokeCode");
           Pokecode.textContent = "Code: #" + PokId;
-          
-          let typeList = document.createElement(ul);
+
+          let typeList = document.createElement("ul");
           typeList.classList.add("PokeTypes");
+
+          let type1 = document.createElement("li");
+          type1.classList.add("type1");
+          type1.textContent = Poktype1;
+
+          let type2 = document.createElement("li");
+          type2.classList.add("type2");
+          type2.textContent = Poktype2 ? Poktype2 : "";
+
+          typeList.appendChild(type1);
+          if (Poktype2) typeList.appendChild(type2);
+          cardBody.append(title, Pokecode, typeList);
+          card.append(img, cardBody);
+
+          container.appendChild(card);
+
+          type1.style.backgroundColor = getComputedStyle(
+            document.documentElement
+          ).getPropertyValue(`--type-${Poktype1}`);
+
+          if (Poktype2) {
+            type2.style.backgroundColor = getComputedStyle(
+              document.documentElement
+            ).getPropertyValue(`--type-${Poktype2}`);
+          }
+
           
-          let type1= document.createElement("li");
-          type1 .classList.add("type1")
-          
-          
-
-
-
-
-console.log(Poktype1)
-
-
-
         });
     });
   });
+
+document.querySelectorAll('.btn-header').forEach(button => {
+    button.addEventListener('click', () => {
+        let type = button.id;
+        let allCards = document.querySelectorAll('.Pokecard');
+
+        allCards.forEach(card => {
+            let types = card.querySelectorAll('.PokeTypes li');
+            let match = false;
+            
+            types.forEach(typeElement => {
+                if (typeElement.textContent.toLowerCase() === type.toLowerCase()) {
+                    match = true;
+                }
+            });
+
+            if (match) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+});
+
+
+document.querySelector('#ver-todos').addEventListener('click', () => {
+  let allCards = document.querySelectorAll('.Pokecard');
+  allCards.forEach(card => {
+      card.style.display = 'block';
+  });
+});
